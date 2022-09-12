cde87b80a05a3d0d29c6339e4b2d10cf792314e4
Alfreyz <sdaliungferry@gmail.com>
2022-09-12 23:11:29 +0700

    update

--- src/examples/Sidenav/SidenavList.vue
+++ src/examples/Sidenav/SidenavList.vue
@@ -4,7 +4,7 @@
     id="sidenav-collapse-main"
   >
     <ul class="navbar-nav">
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <sidenav-item
           url="/dashboard-default"
           :class="getRoute() === 'dashboard-default' ? 'active' : ''"
@@ -62,19 +62,19 @@
             <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
           </template>
         </sidenav-item>
-      </li>
+      </li> -->
       <li class="nav-item">
         <sidenav-item
           url="/user"
           :class="getRoute() === 'user' ? 'active' : ''"
-          navText="user"
+          navText="USER"
         >
           <template v-slot:icon>
             <i class="ni ni-single-02 text-danger text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
-      <li class="mt-3 nav-item">
+      <!-- <li class="mt-3 nav-item">
         <h6
           v-if="this.$store.state.isRTL"
           class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6"
@@ -122,7 +122,7 @@
             <i class="ni ni-collection text-info text-sm opacity-10"></i>
           </template>
         </sidenav-item>
-      </li>
+      </li> -->
     </ul>
   </div>
   <div class="pt-3 mx-3 mt-3 sidenav-footer">
